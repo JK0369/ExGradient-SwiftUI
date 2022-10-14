a99e3dc4d4384ca850c43d1684d0530cd254a69c
jake <palatable7@naver.com>
2022-10-14 09:32:23 +0900

    Initial Commit

---
+++ ExGradient/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExGradient
+//
+//  Created by 김종권 on 2022/10/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExGradient/ExGradientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExGradientApp.swift
+//  ExGradient
+//
+//  Created by 김종권 on 2022/10/14.
+//
+
+import SwiftUI
+
+@main
+struct ExGradientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
